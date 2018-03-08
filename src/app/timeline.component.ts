5b7c05d39c9537e3e40f5b37bb25e4f857806f97
Connell <brian_connell@baxter.com>
2018-03-08 17:20:41 -0600

    format

--- src/app/timeline.component.ts
+++ src/app/timeline.component.ts
@@ -53,24 +53,24 @@ export class TimelineComponent {
     // width:this.elementRef.container.offsetWidth,
     // width: will be 100%,
     // height: will be 100%,
-    scale_factor: 10,                    // How many screen widths wide should the timeline be at first presentation
+    scale_factor: 2,                    // How many screen widths wide should the timeline be at first presentation
     zoom_sequence: [0.5, 1, 2, 5, 9, 15], 	//[0.5, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89], // Array of Fibonacci numbers for TimeNav zoom levels http://www.maths.surrey.ac.uk/hosted-sites/R.Knott/Fibonacci/fibtable.html
     layout: 'landscape',                // portrait or landscape
     timenav_position: 'bottom',         // timeline on top or bottom
-    optimal_tick_width: 1000,            // optimal distance (in pixels) between ticks on axis
+    optimal_tick_width: 50,            // optimal distance (in pixels) between ticks on axis
     //base_class: '',
-    timenav_height: 250,
+    timenav_height: 200,
     timenav_height_percentage: 25,      // Overrides timenav height as a percentage of the screen
     timenav_height_min: 150,            // Minimum timenav height
     marker_height_min: 30,              // Minimum Marker Height
     marker_width_min: 50,              // Minimum Marker Width
     marker_padding: 5,                  // Top Bottom Marker Padding
-    start_at_slide: 1,
+    start_at_slide: 0,
     menubar_height: 100,
     skinny_size: 650,
     relative_date: false,               // Use momentjs to show a relative date from the slide.text.date.created_time field
     use_bc: false,                      // Use declared suffix on dates earlier than 0
-    duration: 500,                     // Slider animation duration
+    duration: 1000,                     // Slider animation duration
     ease: TL.Ease.easeInOutQuint,       // Slider animation type
     dragging: true,
     trackResize: true,
@@ -127,13 +127,19 @@ export class TimelineComponent {
     try{
       if(!this.viewInited){return;}
       var self = this;
-      //var subscription:Subscription = this.getObject().subscribe((object:any)=>{
-        console.log('this url in update trying to find m.url',this.filepath)
       var subscription:Subscription = this.getcsv.getCSV(this.filepath).subscribe(data=> {
           let results = {};
           let events = [];
+          let projectTitle = data[0]['Project Title'];
+          let projectDescription = data[0]['Project Description'];
+          let projectUrl = data[0]['Project Url'];
+          //console.log('lkl',data[0]['Project Title'])
           //console.log('what is data[key]?', this.url, data);
           for (let key in data){
+            
+            //projectTitle = data[key]['Project Title'];
+            //projectDescription = data[key]['Project Description'][0];
+            
             if(data[key]['Headline']){
             if(data[key]['Video Link']){
               events.push({
@@ -149,16 +155,10 @@ export class TimelineComponent {
                 },
                 'group':data[key]['Group'],
                 "text": {
-                  'headline': data[key]['Headline'],
-                  'text': "<video width='200' height='200' controls>"+
-                  "<source src="+data[key]['Video Link']+ " type='video/mp4'></video>"+ "<span style='background-color:"+data[key]['Background']+"'>"+data[key]['Text']+"</span>"+"<img style='max-height: 100px; max-width: 100px;' src='"+data[key]['Picture Link'] +"' class='media-image'>"
+                  'headline':data[key]['Headline'] ,
+                  'text': "<table width='100%' class='ms-rteTable-default' cellspacing='0'><tbody><tr><td class='ms-rteTable-default' style='width: 10%;'>"+data[key]['Background']+"</td><td class='ms-rteTable-default' style='width: 50%;'>"​+"<video controls>"+
+                  "<source src="+data[key]['Video Link']+ " type='video/mp4'></video>"+"</td><td class='ms-rteTable-default' style='width: 40%;'>​"+data[key]['Text']+"</td></tr></tbody></table>"
                 },
-                  "media": {
-                    "url": '',
-                    "caption": data[key]['Media Caption'],
-                    "credit": data[key]['Media Credit'],
-                    "thumbnail":data[key]['Media Thumbnail']
-                  },
                 'Type':data[key]['Type'],
                 'Background':data[key]['Background']
               })
@@ -177,14 +177,10 @@ export class TimelineComponent {
                 'group':data[key]['Group'],
                 "text": {
                   'headline': data[key]['Headline'],
-                  'text': "<p>"+data[key]['Text']+"</p>"+"<img style='max-height: 100px; max-width: 100px;' src='"+data[key]['Picture Link'] +"' class='media-image'>"
+                  'text': "<span style='color:"
+                  +data[key]['Background']+"'>"
+                  +data[key]['Text']+"</span>"
                 },
-                  "media": {
-                    "url": '',
-                    "caption": data[key]['Media Caption'],
-                    "credit": data[key]['Media Credit'],
-                    "thumbnail":data[key]['Media Thumbnail']
-                  },
                 'Type':data[key]['Type'],
                 'Background':data[key]['Background']
               })
@@ -199,12 +195,12 @@ export class TimelineComponent {
                     "media": {
                         "caption": "",
                         "credit": "",
-                        "url": "http://2.bp.blogspot.com/-dxJbW0CG8Zs/TmkoMA5-cPI/AAAAAAAAAqw/fQpsz9GpFdo/s1600/voyage-dans-la-lune-1902-02-g.jpg",
+                        "url": projectUrl,
                         "thumb": 	""
                     },
                       "text": {
-                          "headline": "Finding the Problem",
-                          "text": "<p>Mapping the therapy to better understanding </p>"
+                          "headline": projectTitle,
+                          "text": projectDescription
                       }
               },
             "events":events,
@@ -232,46 +228,6 @@ export class TimelineComponent {
       );
     }catch(e){console.warn(e);}
   }
-
-  /*
-orginal
-          try{
-            if(typeof object !== 'string'){
-              if (object.events.length == 0){return;}
-            }
-            self.timeline = new TL.Timeline(self.id, object,this.defaultOptions);
-            //self.currentEvents = object.events;
-            self.currentEvents = this.results.events;
-            let temp = self.currentEvents[0]?self.currentEvents[0]:null;
-            self.setSelected(temp);
-          }catch(e){console.warn(e);}
-        },
-        (error:any)=>{console.warn(error);},
-        () => {
-          if(subscription){
-            subscription.unsubscribe();
-          }
-        }
-      );
-    }catch(e){console.warn(e);}
-  }
-  */
-  
-  // replace with
-/*   private getObject(): Observable<any> {
-        //if (this.url !== '') {
-          //return this.http.get(this.url);
-        //}
-        return new Observable((observer: Observer<any>) => {
-          try {
-            observer.next({
-              scale: this.results.scale,
-              events: this.results.events
-            });
-            observer.complete();
-          } catch (e) {console.warn(e);}
-        });
-      } */
       
   private setSelected(selected:Array<any>){
     if(selected){
@@ -295,72 +251,3 @@ orginal
   }
 
 }
-
-/*   public getCSV(this.url).subscribe(data=> {
-    let events = [];
-    for (let key in data){
-      events.push({
-        "start_date": {
-          //'year':data[key]['Year'],
-          //'month':data[key]['Month'],
-          //'day':data[key]['Day'],
-          //'Time':data[key]['Time'],
-          'hour':data[key]['Hour'],
-          'minute':data[key]['Minute'],
-          //"second": data[key]['Second'],
-          //'millisecond':data[key]['Millisecond'],
-          "format": data[key]['Display Date']
-        },
-        "end_date":{
-          //'year':data[key]['End Year'],
-          //'month':data[key]['End Month'],
-          //'day':data[key]['End Day'],
-          //'End Time':data[key]['End Time'],
-          'hour':data[key]['End Hour'],
-          'minute':data[key]['End Minute'],
-          //"second": data[key]['End Second'],
-          //'millisecond':data[key]['End Millisecond'],
-          "format": data[key]['Display Date']
-        },
-        'group':data[key]['Group'],
-        "text": {
-          'headline': data[key]['Headline'],
-          'text':data[key]['Text']
-        },
-         "media": {
-              "caption": data[key]['Media Caption'],
-              "credit": data[key]['Media Credit'],
-              "url": data[key]['url'],
-              "thumb": ""
-          },
-           "media": {
-              //"caption": data[key]['Media Caption'],
-              //"credit": data[key]['Media Credit'],
-              //"url": data[key]['Media'],
-              //"thumb": data[key]['Media Thumbnail']
-          },  
-        'Type':data[key]['Type'],
-        'Background':data[key]['Background']
-      })
-    }
-
-    this.results = {
-    "title": {
-              "media": {
-                  "caption": "",
-                  "credit": "",
-                  "url": "http://2.bp.blogspot.com/-dxJbW0CG8Zs/TmkoMA5-cPI/AAAAAAAAAqw/fQpsz9GpFdo/s1600/voyage-dans-la-lune-1902-02-g.jpg",
-                  "thumb": 	""
-              },
-                "text": {
-                    "headline": "Welcome to TimelineJS",
-                    "text": "<p>TimelineJS is an open-source tool that enables you to build visually-rich interactive timelines and is available in 40 languages.</p><p>You're looking at an example of one right now.</p><p>Click on the arrow to the right to learn more.</p>"
-                }
-        },
-      "events":events,
-      //"eras":"",
-      "scale": "human",
-
-      }
- 
-  }); */
